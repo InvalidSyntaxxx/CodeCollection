88d010051e6511f57c056020d8676cbe271a3898
Redamancy9189 <1791081494@qq.com>
2023-01-07 23:08:12 +0800

    feat: Vue3框架

--- 前端/Web智能编程/end-term/Front-end/script/main.js
+++ 前端/Web智能编程/end-term/Front-end/script/main.js
@@ -4,22 +4,29 @@
  * @Author: 王远昭
  * @Date: 2023-01-05 23:10:46
  * @LastEditors: 王远昭
- * @LastEditTime: 2023-01-07 18:51:49
+ * @LastEditTime: 2023-01-07 22:31:32
  */
-let product = []
-axios.get("http://localhost:53000/products")
-.then((res)=>{
-    let data = res.data
-    for(let i=0;i<8;i++){
-        let template = `
-        <div class="product">
-        <div class="imgcontainer">
-            <img src="../data/img/`+data[i]['img']+`" alt="">
-        </div>
-        <h3 class="name">`+data[i]['name']+`</h3>
-        <p class="description">`+data[i]['description'].substring(0,16)+`...</p>
-        <p class="price">`+data[i]['price']+`元</p>
-        </div>`;
-        $(".right").append(template);
+
+const { createApp } = Vue
+
+createApp({
+    data(){
+        return {
+            products: []
+        }
+    },
+    mounted(){
+        this.getProduct(8);
+    },
+    methods: {
+        getProduct(limit){
+            axios.get("http://localhost:53000/products?_limit="+limit)
+            .then((res)=>{
+               this.products = res.data
+            })
+        }
     }
-})
+}).mount('#app');
+
+
+
