72b7abef9e9f413a7ccbb936590e6358035902d7
Redamancy9189 <1791081494@qq.com>
2023-01-07 23:30:55 +0800

    feat: 增加"其它"栏

--- 前端/Web智能编程/end-term/Front-end/script/main.js
+++ 前端/Web智能编程/end-term/Front-end/script/main.js
@@ -1,32 +1,39 @@
 /*
- * @Descripttion: 
- * @version: 
+ * @Descripttion:
+ * @version:
  * @Author: 王远昭
  * @Date: 2023-01-05 23:10:46
  * @LastEditors: 王远昭
- * @LastEditTime: 2023-01-07 22:31:32
+ * @LastEditTime: 2023-01-07 23:29:22
  */
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
 createApp({
-    data(){
-        return {
-            products: []
-        }
+  data() {
+    return {
+      products: [],
+      others: [],
+    };
+  },
+  mounted() {
+    this.getProduct(8);
+    this.getOther(8);
+  },
+  methods: {
+    getProduct(limit) {
+      axios
+        .get("http://localhost:53000/products?_limit=" + limit)
+        .then((res) => {
+          this.products = res.data;
+        });
     },
-    mounted(){
-        this.getProduct(8);
+    getOther(limit) {
+      axios
+        .get("http://localhost:53000/products?productId_gte=" + (limit+1))
+        .then((res) => {
+          this.others = res.data;
+        });
     },
-    methods: {
-        getProduct(limit){
-            axios.get("http://localhost:53000/products?_limit="+limit)
-            .then((res)=>{
-               this.products = res.data
-            })
-        }
-    }
-}).mount('#app');
-
-
-
+  },
+}).mount("#app");
